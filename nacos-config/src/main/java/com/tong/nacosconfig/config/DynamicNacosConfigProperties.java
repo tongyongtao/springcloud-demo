f55b5b0cead189c26053c6af63815aaa7d941afb
tongyt <tongyongtao@foonsu.com>
2025-02-26 17:39:03 +0800

    使用nacos作为项目的配置中心

---
+++ nacos-config/src/main/java/com/tong/nacosconfig/config/DynamicNacosConfigProperties.java
@@ -0,0 +1,20 @@
+package com.tong.nacosconfig.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tongyt
+ * @date 2025-02-26
+ */
+@RefreshScope
+@Data
+@Component
+public class DynamicNacosConfigProperties {
+
+    @Value("${tong.name}")
+    private String name;
+
+}
---
+++ nacos-config/src/main/java/com/tong/nacosconfig/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.tong.nacosconfig.controller;
+
+import com.tong.nacosconfig.config.DynamicNacosConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tongyt
+ * @date 2025-02-26
+ */
+@RestController
+public class HelloController {
+
+    @Autowired
+    private DynamicNacosConfigProperties dynamicNacosConfigProperties;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello " + dynamicNacosConfigProperties.getName();
+    }
+
+}
